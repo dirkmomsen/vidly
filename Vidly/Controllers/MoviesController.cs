dbb817ddd86a800d126a4737e21201f49dd132a8
dirkmomsen <dirkmomsen@hotmail.com>
2019-04-06 15:29:44 +1300

    Added a link to the Movie name for editing.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -26,9 +26,10 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
+            /*var movies = _context.Movies.Include(m => m.Genre).ToList();
 
-            return View(movies);
+            return View(movies);*/
+            return View();
         }
 
 
